351ed50473aa73ffd6815e1419b5ac223e5e20fd
Hannah Grinton <hannahgrinton@gmail.com>
2019-10-15 13:14:39 -0300

    added components required and imported styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { JumpComponent } from './jump/jump.component';
+import { MainComponent } from './main/main.component';
+import { CommentComponent } from './comment/comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JumpComponent,
+    MainComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule
